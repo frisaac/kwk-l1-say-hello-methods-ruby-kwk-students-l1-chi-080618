b5574142e99bb562e45a7871c6435ec874382c8f
frisaac <frisaac@stu.naperville203.org>
2018-08-07 16:26:54 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Build your say_hello method here
 def say_hello(person_name = "Ruby Programmer")
-  "Hello #{person_name}!"
+  puts "Hello #{person_name}!"
 end
 
-puts say_hello("Gabriela")
+say_hello("Gabriela")
   
